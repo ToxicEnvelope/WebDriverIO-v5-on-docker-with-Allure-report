27f88fc6024a2d2222e76c8f335629f9171442d6
Yahav N. Hoffman <23465872+ToxicEnvelope@users.noreply.github.com>
2020-08-01 20:48:12 +0300

    add config modules, files and license
    jsconfig, LICENSE, dependencies and wdio configurations

---
+++ config/wdio/base.conf.js
@@ -0,0 +1,69 @@
+
+require('module-alias/register');
+require('@babel/register')({
+  presets: [[
+    '@babel/preset-env',
+    { targets: { node: 8 } },
+  ]],
+  babelrc: false,
+});
+
+const video = require('wdio-video-reporter');
+
+const config = {
+  // =============================
+  //      Reporter options
+  // =============================
+  outputDir: './_results_',
+  reporters: [
+    'spec',
+    [video, {
+      saveAllVideos: false,       // If true, also saves videos for successful test cases
+      videoSlowdownMultiplier: 3, // Higher to get slower videos, lower for faster videos [Value 1-100]
+      videoRenderTimeout: 5,      // Max seconds to wait for a video to finish rendering
+    }],
+    ['allure', {
+      outputDir: './_results_/allure-raw',
+      disableWebdriverStepsReporting: true,
+      disableWebdriverScreenshotsReporting: true,
+    }],
+  ],
+
+  // =============================
+  //      Console options
+  // =============================
+  logLevel: 'info', // trace | debug | info | warn | error | silent
+  coloredLogs: true,
+  deprecationWarnings: true,
+  
+
+  // =============================
+  //        Base Options
+  // =============================
+  baseURL: 'http://localhost:8080',
+  specs: [
+    './src/scenarios/**/*.e2e.js',
+  ],
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+      defaultTimeoutInterval: 100000,
+  },
+  sync: true,
+  bail: 0,
+  maxInstances: 10,
+  capabilities: [
+    {
+      maxInstances: 1,
+      browserName: 'chrome',
+    }
+  ],
+  runner: 'local',
+  services: undefined,
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+};
+
+module.exports = {
+  config,
+};
---
+++ config/wdio/wdio-e2e.conf.js
@@ -0,0 +1,33 @@
+const base = require('./base.conf.js');
+
+const config = {
+  specs: [
+    './src/**/*.e2e.js'
+  ],
+
+  capabilities: [
+    {
+      maxInstances: 1,
+      browserName: 'chrome',
+      "goog:chromeOptions": {
+        prefs: {
+          'profile.managed_default_content_settings.popups' : 2,
+          'profile.managed_default_content_settings.notifications' : 2,
+        }
+      }
+    },
+  ],
+
+  // Setup the browser window
+  before: (capabilities, specs) => {
+    browser.setWindowPosition(0, 0);
+    browser.setWindowSize(1320, 768);
+  },
+};
+
+module.exports = {
+  config: {
+    ...base.config,
+    ...config,
+  },
+};
